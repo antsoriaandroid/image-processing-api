62fd4783382c437339e97a516a6f00fb44f344f0
Antonio S <antsoriaandroid@gmail.com>
2022-07-06 23:07:39 +0200

    Routes structure added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import routes from './routes/router';
 
 const app = express();
 const port = 3000;
 
-app.get('/api', (req, res) => {
+//api/images
+app.use('/api', routes);
+
+
+/*app.get('/api', (req, res) => {
     res.send('Hello, world!');
 });
-
+*/
 app.listen(port, ()=> {
     console.log(`server started at localhost:${port}`)
 });
---
+++ src/routes/router.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import images from './api/images';
+
+const router = express.Router();
+
+router.get('/',(req, res)=>{
+    res.send('Main API route');
+});
+
+router.use('/images', images);
+
+export default router;
